bde7e4857d826318595cbb64ec32d591d78cb8d5
CosWind <xy2491259@gmail.com>
2012-08-06 15:52:46 +0800

    Add Unique:username.

--- user.sql
+++ user.sql
@@ -7,15 +7,15 @@ CREATE TABLE `user` (
   `name`      varchar(40)  NOT NULL,
   `username`  varchar(40)  NOT NULL,
   `password`  varchar(40)  NOT NULL,
-  `type`      varchar(20)   NOT NULL,
-  `phone`     varchar(40)   NOT NULL,
+  `type`      varchar(20)  NOT NULL,
+  `phone`     varchar(40)  NOT NULL,
   `address`   varchar(20)  DEFAULT "",
   `introduce` varchar(200) DEFAULT "",
   `picture`   varchar(30)  NOT NULL,
   `score`     tinyint(4)   DEFAULT 0,
+  UNIQUE (`username`),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 
 LOCK TABLES `user` WRITE;
 
